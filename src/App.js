8425c58c3ebd9fa0c7803e2c0262bb5cf19464ba
Sergey1976 <sg0996613153@gmail.com>
2025-02-18 15:52:06 +0200

    add component Header to App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { useEffect } from 'react';
 import { useTelegram } from './hooks/useTelegram';
 
-import Header from 'components/Header/Header';
+import Header from './components/Header/Header';
 
 function App() {
     const { onToggleButton, tg } = useTelegram();
